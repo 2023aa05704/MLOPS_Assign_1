1957d68c7ae7f39995f60bdd8c3d47594803e3cd
Anshul Gupta <agupta@akamai.com>
2025-01-25 12:26:21 +0530

    trying port 80

--- src/app.py
+++ src/app.py
@@ -60,4 +60,4 @@ def predict():
 # Start the Flask application
 if __name__ == '__main__':
     app.logger.info("Starting Flask application.")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=80)
